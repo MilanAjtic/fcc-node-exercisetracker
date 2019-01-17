409098ef44f014043933c367689e6ba11a754787
Milan <milanajtic@gmail.com>
2019-01-17 02:16:50 +0100

    added comments

--- server.js
+++ server.js
@@ -84,6 +84,14 @@ app.post('/api/exercise/add', (req, res) => {
   })
 })
 
+// Prethodni task (br. 3) je primer kako moze da se uradi query bez da se radi populate()
+
+// Doda se novi Exercise, onda se na osnovu userId-a (koji je unesen u formi), pronadje medju Users taj koji nam treba, i uzmemo njegov username
+
+// U ovom slucaju nije neophodno da se svaki novi Exercise push-uje kod User-a, ali je ok da bi baza bila konzistentna
+
+
+
 // Tasks 4 and 5
 app.get('/api/exercise/log', (req, res) => {
   const obj = {
@@ -136,6 +144,16 @@ app.get('/api/exercise/log', (req, res) => {
         )
     }).catch(err => console.error(err))
 })
+
+// Prethodni taskovi (br. 4 i 5) su primer kako moze da se uradi query pomocu populate()
+
+// Trazimo Exercises koji zadovoljavaju zadate parametre, onda uradimo populate() i u atribut user unesemo ceo sadrzaj odredjenog User-a sa kojim su nase Exercises povezane (preko userId-a)
+
+// Nama je bio potreban samo username, tako da nije bilo neophodno da se radi preko populate(), vec smo mogli kao i u tasku 3, da uradimo User.find() i tako nadjemo User-a koji ima zadati userId, uzmemo njegov _id, i stavimo ga u response zajedno sa nizom Exercises koje smo vec pre toga nasli
+
+// Ceo zadatak je mogao da se uradi bez refs, pomocu dve nezavisne sheme User i Exercise, bilo bi dovoljno samo da se u ExerciseSchema nalazi atribut user koji bi sadrzao _id odredjenog User-a
+
+
 
 // za glitch mora da bude process.env.PORT ili 3000
 const listener = app.listen(3001, () => {
